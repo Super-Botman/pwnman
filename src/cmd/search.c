9de5d07f8152a24515181984ed4d7df4eab70c4a
Anska <103422661+0011101100101001@users.noreply.github.com>
2026-09-10 17:04:16 +0200

    fix: to_find var

--- src/cmd/search.c
+++ src/cmd/search.c
@@ -1,7 +1,7 @@
 #include "pwnman.h"
 
-void search(struct db *db, char *searched) {
-  if (is_database_empty(db) || !has_database(db) && !has_arguments(arg)) {
+void search(struct db *db, char *to_find) {
+  if (is_database_empty(db) || !has_database(db) && !has_arguments(to_find)) {
       return; 
     }
 
@@ -9,7 +9,7 @@ void search(struct db *db, char *searched) {
   for (size_t i = 0; i < db->count; i++, entry++) {
     char title[TITLE_MAX + 1];
     get_title(entry, title);
-
+    if ()
   }
 }
 
